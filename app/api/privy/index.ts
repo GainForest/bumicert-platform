be1c0b624b211dc45e37bfbbe1930c8381dff9b7
Satyam Mishra <satyam1308mishra@gmail.com>
2025-09-30 20:15:15 +0530

    fix: fix a build error

--- app/api/privy/index.ts
+++ app/api/privy/index.ts
@@ -2,7 +2,7 @@ import { PrivyClient } from "@privy-io/server-auth";
 
 const appId = process.env.NEXT_PUBLIC_PRIVY_APP_ID;
 const clientId = process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID;
-const privySecret = process.env.PRIVY_SECRET;
+const privySecret = process.env.PRIVY_APP_SECRET;
 
 if (!appId || !clientId || !privySecret) {
   throw new Error("Privy app ID, client ID, or secret are not set");
