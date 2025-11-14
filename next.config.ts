3f7991d4a7fc627d4031b3aeed0df96f39218663
Satyam Mishra <satyam1308mishra@gmail.com>
2025-11-14 19:00:43 +0530

    fix: use webpack to fix build errors

--- next.config.ts
+++ next.config.ts
@@ -10,11 +10,50 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.externals["@solana/web3.js"] = "commonjs @solana/web3.js";
-    config.externals["@solana/spl-token"] = "commonjs @solana/spl-token";
+  // Configure for Turbopack (default in Next.js 16)
+  turbopack: {},
+  // Webpack config for when using --webpack flag
+  webpack: (config, { webpack }) => {
+    // Ignore test files and test dependencies using webpack's IgnorePlugin
+    config.plugins = [
+      ...(config.plugins || []),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^tap$/,
+      }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^tape$/,
+      }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^why-is-node-running$/,
+      }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^@react-native-async-storage\/async-storage$/,
+      }),
+      new webpack.IgnorePlugin({
+        checkResource(resource: string) {
+          // Ignore test files in node_modules
+          if (resource.includes("node_modules")) {
+            if (
+              resource.includes("/test/") ||
+              resource.match(/\.test\.(js|mjs|ts|tsx)$/)
+            ) {
+              return true;
+            }
+          }
+          return false;
+        },
+      }),
+    ];
+
     return config;
   },
+  // Externalize test-related packages to prevent bundling
+  serverExternalPackages: [
+    "tap",
+    "tape",
+    "why-is-node-running",
+    "@react-native-async-storage/async-storage",
+  ],
 };
 
 export default nextConfig;
