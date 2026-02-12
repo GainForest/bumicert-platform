cbb07592152c3b32a6483d9a095a154beaca3aa7
kzoeps <kzoepa@gmail.com>
2026-02-12 18:23:45 +0600

    feat: use gainforest.id for prod

--- config/gainforest-sdk.ts
+++ config/gainforest-sdk.ts
@@ -1,9 +1,9 @@
 import { SupportedPDSDomain } from "gainforest-sdk";
 import { createTRPCClient } from "gainforest-sdk/client";
 
-export const allowedPDSDomains = [
-  "climateai.org",
-] satisfies SupportedPDSDomain[];
+export const allowedPDSDomains = (process.env.VERCEL_TARGET_ENV === "production" ? ["gainforest.id"] : [
+  "gainforest.id",
+]) satisfies SupportedPDSDomain[];
 export type AllowedPDSDomain = (typeof allowedPDSDomains)[number];
 
 export const trpcClient = createTRPCClient<AllowedPDSDomain>("/api/trpc");
