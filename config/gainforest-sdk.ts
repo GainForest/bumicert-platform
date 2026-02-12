b91165271e6708586805dca043754cc08a8137fc
Satyam Mishra <satyam1308mishra@gmail.com>
2026-02-12 19:22:41 +0530

    fix: fix build error related to using different PDS domains

--- config/gainforest-sdk.ts
+++ config/gainforest-sdk.ts
@@ -1,9 +1,9 @@
 import { SupportedPDSDomain } from "gainforest-sdk";
 import { createTRPCClient } from "gainforest-sdk/client";
 
-export const allowedPDSDomains = (process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? ["gainforest.id"] : [
+export const allowedPDSDomains: SupportedPDSDomain[] = (process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? ["gainforest.id"] : [
   "climateai.org",
-]) satisfies SupportedPDSDomain[];
+]);
 export type AllowedPDSDomain = (typeof allowedPDSDomains)[number];
 
 const resolvedVercelEnv =
