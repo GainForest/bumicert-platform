6b847b94483ff6f18f469318d77ae7d46d399ef8
Satyam Mishra <satyam1308mishra@gmail.com>
2026-02-19 21:16:45 +0530

    Add form wrapper to StepIntro for Enter key submission (ecocertain-2-97a.3)

--- config/gainforest-sdk.ts
+++ config/gainforest-sdk.ts
@@ -1,13 +1,13 @@
 import { SupportedPDSDomain } from "gainforest-sdk";
 import { createTRPCClient } from "gainforest-sdk/client";
 
-export const allowedPDSDomains: SupportedPDSDomain[] = (process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? ["gainforest.id"] : [
-  "climateai.org",
-]);
-export type AllowedPDSDomain = (typeof allowedPDSDomains)[number];
-
 const resolvedVercelEnv =
   process.env.NEXT_PUBLIC_VERCEL_ENV ?? process.env.VERCEL_ENV;
+
+export const allowedPDSDomains: SupportedPDSDomain[] =
+  resolvedVercelEnv === "production" ? ["gainforest.id"] : ["climateai.org"];
+
+export type AllowedPDSDomain = (typeof allowedPDSDomains)[number];
 
 export const defaultPdsDomain: AllowedPDSDomain =
   resolvedVercelEnv === "production" ? "gainforest.id" : "climateai.org";
