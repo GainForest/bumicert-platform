9069b21687b2b7b72da34b7bbc277cbdfda41070
Satyam Mishra <satyam1308mishra@gmail.com>
2026-02-20 11:09:19 +0530

    Move explore search/filters from header to page body (bumicerts-platform-jbv.8)

---
+++ .changeset/move-explore-filters-to-page-body.md
@@ -0,0 +1,5 @@
+---
+"bumicerts": patch
+---
+
+Moved search and filters on the Explore page from the header into the page body, and added a hero section with title, subtitle, and bumicert count
--- app/(marketplace)/explore/page.tsx
+++ app/(marketplace)/explore/page.tsx
@@ -3,12 +3,16 @@ import React, { Suspense } from "react";
 import Bumicerts from "./_components/Bumicerts";
 import HeaderContent from "./_components/HeaderContent";
 import ExploreHydrator from "./_components/ExploreHydrator";
+import ExploreHero from "./_components/ExploreHero";
+import ExploreFilters from "./_components/ExploreFilters";
 
 const ExplorePage = () => {
   return (
     <Container>
       <HeaderContent />
       <ExploreHydrator>
+        <ExploreHero />
+        <ExploreFilters />
         <Suspense>
           <Bumicerts />
         </Suspense>
